f2443bcb99d9863b11a53a62bc759b1d4d055642
kooqooo <koo9717@gmail.com>
2024-03-18 20:58:26 +0900

    chore: 불필요한 주석 제거
    #7

--- front/jobits/0_introduction.py
+++ front/jobits/0_introduction.py
@@ -14,37 +14,35 @@ NEXT_PAGE = 'home'
 
 st.title('헬로자비스 홈페이지')
 
-## input_form
 input_form, start_button = st.columns([1,2]) # 노션 컬럼처럼 열을 나눠서 할수있게 해주는것
-
-    ## start_button
+# start_button = st.button
 with start_button:
-        start_button.markdown(f''' 
-                              <div class = 'main_message'> JOBits <br></div> 
-                              ''', 
-                              unsafe_allow_html=True )
-        #
-        ### 필요사항 따라 버튼 클릭시 안내 문구 생성
-        if start_button.button('예상 질문 확인하기'):
-            ### 유저 고유 폴더 생성
+    start_button.markdown(f''' 
+                            <div class = 'main_message'> JOBits <br></div> 
+                            ''', 
+                            unsafe_allow_html=True )
+    #
+    ### 필요사항 따라 버튼 클릭시 안내 문구 생성
+    if start_button.button('예상 질문 확인하기'):
+        ### 유저 고유 폴더 생성
+        
+        # if check_list:
+        #     start_button.markdown(f'''
+        #                           <p class = 'check_message'>{', '.join(check_list)}{josa[-1]} 필요해요! </p>
+        #                           ''',
+        #                           unsafe_allow_html=True)
+        # else:
+        switch_page(NEXT_PAGE)
+        st.session_state.logger.info(f"check_essential | Pass")
             
-            # if check_list:
-            #     start_button.markdown(f'''
-            #                           <p class = 'check_message'>{', '.join(check_list)}{josa[-1]} 필요해요! </p>
-            #                           ''',
-            #                           unsafe_allow_html=True)
-            # else:
-            switch_page(NEXT_PAGE)
-            st.session_state.logger.info(f"check_essential | Pass")
-                
-                
-        if start_button.button('모의면접 시작하기'):
-            ### 유저 고유 폴더 생성
-            # if check_list:
-            #     start_button.markdown(f'''
-            #                           <p class = 'check_message'>{', '.join(check_list)}{josa[-1]} 필요해요! </p>
-            #                           ''',
-            #                           unsafe_allow_html=True)
-            # else:
-            switch_page(NEXT_PAGE)
-            st.session_state.logger.info(f"check_essential | Pass")
+            
+    if start_button.button('모의면접 시작하기'):
+        ### 유저 고유 폴더 생성
+        # if check_list:
+        #     start_button.markdown(f'''
+        #                           <p class = 'check_message'>{', '.join(check_list)}{josa[-1]} 필요해요! </p>
+        #                           ''',
+        #                           unsafe_allow_html=True)
+        # else:
+        switch_page(NEXT_PAGE)
+        st.session_state.logger.info(f"check_essential | Pass")
