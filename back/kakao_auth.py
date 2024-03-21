749fd9228d7f05cc838e1672cab9be8d1cc70a21
maxseats <oooooo123456@naver.com>
2024-03-22 03:12:49 +0900

    feat: 로그인 시 쿠키 저장 + access_token 불러오는 엔드포인트 구현(/items)

--- back/kakao_auth.py
+++ back/kakao_auth.py
@@ -3,7 +3,8 @@ from typing import Optional
 
 import requests
 from config import REDIRECT_URI, REST_API_KEY
-from fastapi import APIRouter, HTTPException, status
+from fastapi import APIRouter, Request
+from fastapi.responses import JSONResponse
 from starlette.responses import RedirectResponse
 
 from user_authorization import verify_token
@@ -11,7 +12,6 @@ from share_var import set_shared_var  # 공유 변수 관련 함수 불러오기
 
 router = APIRouter()
 
-ACCESS_TOKEN = None # 토큰 저장
 ID_TOKEN = None  # ID 토큰 : 로그인 여부 확인용
 
 @router.get("/kakao")
@@ -33,10 +33,12 @@ async def kakaoAuth(code: Optional[str] = "NONE"):
     ID_TOKEN = str(_result["id_token"])  # ID 토큰 저장
 
     end_point = "/launch_streamlit_app"  # 이동할 페이지 지정
+    response = RedirectResponse(url=f"{end_point}?access_token={ACCESS_TOKEN}")
     
-    return RedirectResponse(url=f"{end_point}?access_token={ACCESS_TOKEN}")
-
-
+    # 쿠키 설정하기
+    response.set_cookie(key="access_token", value=ACCESS_TOKEN)  
+    
+    return response
 @router.get("/kakaoLogout")
 def kakaoLogout():
     url = "https://kapi.kakao.com/v1/user/unlink"
@@ -62,6 +64,15 @@ async def get_user_info():
         return user_info
     except Exception as e:
         return None
+    
+
+@router.get("/items")
+async def read_items(request: Request):
+    
+    # print("쿠키: ", request.cookies.get('sessionKey'))
+    print("쿠키: ", request.cookies.get('access_token'))
+    return {"access_token":request.cookies.get('access_token')}
+
 
 def check_login():
     """
