69c3505c116183bb31464ef26cd587051cd5b5fc
구희찬 <koo9717@gmail.com>
2024-08-10 16:46:43 +0900

    fix: 오타 수정

--- back/managers/mongo_config.py
+++ back/managers/mongo_config.py
@@ -3,7 +3,7 @@ from pymongo import MongoClient
 from gridfs import GridFSBucket
 from dotenv import load_env
 
-load_env(overide=True)
+load_env(override=True)
 
 # 환경 변수로부터 MongoDB 설정 읽기
 username = os.getenv("MONGO_USERNAME", "admin")
